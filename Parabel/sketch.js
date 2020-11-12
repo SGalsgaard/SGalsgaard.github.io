c2572f85a244e85c9833b8f64ca41404b05ea19c
SGalsgaard <72203483+SGalsgaard@users.noreply.github.com>
2020-11-12 14:18:00 +0100

    Parablens linjer er sat

--- Parabel/sketch.js
+++ Parabel/sketch.js
@@ -1,7 +1,16 @@
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(600, 800);
+  fill(0, 0, 0)
 }
 
 function draw() {
   background(220);
+  circle(20, 20, 15)
+  circle(580, 20, 15)
+  circle(width/2, mouseY, 15)
+  line(20, 20, width/2, mouseY)
+  line(580, 20, width/2, mouseY)
+
+  let n = map(mouseX, 20, width, 2, 10, true);
+  n = round(n)
 }
